6deeed41bd16e7d7c646a51a8dc1caf7409d7896
Milton Rafa <miltonrs.dev@gmail.com>
2025-10-30 23:20:38 -0300

    Fix: configurar CSP para permitir Google Translate e resolver erro de stylesheet

--- portal_noticias/config/server.js
+++ portal_noticias/config/server.js
@@ -7,6 +7,19 @@ var expressValidator = require('express-validator');
 var app = express();
 app.set('view engine', 'ejs');
 
+
+// Configurar Content Security Policy para permitir Google Translate
+app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', 
+        "default-src 'self'; " +
+        "style-src 'self' 'unsafe-inline' https://www.gstatic.com; " +
+        "script-src 'self' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com; " +
+        "img-src 'self' data: https:; " +
+        "connect-src 'self' https://translate.googleapis.com;"
+    );
+    next();
+});
+
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
