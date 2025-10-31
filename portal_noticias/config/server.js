8ba3471c6ec38b04701c011e041130810441ba78
Milton Rafa <miltonrs.dev@gmail.com>
2025-10-30 23:30:22 -0300

    HOTFIX: remover CSP que quebrou o servidor - voltar ao básico

--- portal_noticias/config/server.js
+++ portal_noticias/config/server.js
@@ -7,19 +7,6 @@ var expressValidator = require('express-validator');
 var app = express();
 app.set('view engine', 'ejs');
 
-
-// Configurar CSP totalmente permissivo para projeto de portfólio
-app.use((req, res, next) => {
-    // Remove headers CSP existentes
-    res.removeHeader('Content-Security-Policy');
-    res.removeHeader('X-Content-Security-Policy');
-    res.removeHeader('X-WebKit-CSP');
-    
-    // Define CSP completamente permissivo
-    res.setHeader('Content-Security-Policy', "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;");
-    next();
-});
-
 app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
