22452bcdfd9f82d8e2e34124d7025f60759057be
Milton Rafa <miltonrs.dev@gmail.com>
2025-11-01 03:15:05 -0300

    Alterando dados de acesso ao banco

--- portal_noticias/config/dbConnection.js
+++ portal_noticias/config/dbConnection.js
@@ -8,15 +8,15 @@ var mysql = require('mysql2');
 var connMysql = function(){
     console.log('Conexão com banco de dados MySQL estabelecida');
     return mysql.createConnection({
-		/*para desenvolvimento local*/
+		/*para desenvolvimento local
 		host: process.env.DB_HOST || 'localhost',
 		user: process.env.DB_USER || 'portal_app',
-        password : process.env.DB_PASS || 'portal123',
+        password : process.env.DB_PASS || 'portal123',*/
 
-		/*para produção no railway
+		/*para produção no railway*/
         host     : process.env.DB_HOST || 'mysql.railway.app',
         user     : process.env.DB_USER || 'milton',
-        password : process.env.DB_PASS || '28032014Aa',*/
+        password : process.env.DB_PASS || '28032014Aa',
         database : process.env.DB_NAME || 'railway',
         port     : process.env.DB_PORT || 3306
     });
